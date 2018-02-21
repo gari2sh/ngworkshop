995a72a82649bb676bc96fd399a48ade06bea69e
Garima Guru <garimaguru@gmail.com>
2018-02-21 10:32:43 -0500

    Lab3 Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatListModule, MatCardModule, MatSlideToggleModule, MatDialogModule,
+  MatIconModule, MatInputModule, MatSnackBarModule, MatTabsModule,
+  MatButtonModule,MatLineModule, MatToolbarModule } from '@angular/material';
+ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { CollectionComponent } from './collection/collection.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CollectionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    MatListModule,
+    MatTabsModule,
+    MatSnackBarModule,
+    MatDialogModule,
+    MatCardModule,
+    MatIconModule,
+    MatSlideToggleModule,
+    MatButtonModule,
+    MatLineModule,
+    MatInputModule,
+    MatToolbarModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
